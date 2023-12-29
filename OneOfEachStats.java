bc0a222f3bfc10311578bc619e5187f5ef03e792
unknown <omer.lasry@post.runi.ac.il>
2023-12-29 22:39:20 +0200

    fixed program

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -14,7 +14,48 @@ public class OneOfEachStats {
 		int seed = Integer.parseInt(args[1]);
 		// Initailizes a random numbers generator with the given seed value
         Random generator = new Random(seed);  
-		
+		boolean b = false, g =false;
+		int sum = 0 , n2 =0 , n3 =0 ,n4 = 0 ;
+		double avg =0;
+		for(int i = 1 ; i<=T ; i ++)
+		{
+            b = false ;
+            g = false;
+		while((b&&g)==false)
+		{
+			if(generator.nextDouble()>0.5)
+			{
+				b = true;
+				
+			}
+			else
+			{
+				g = true;
+				
+			}
+			avg++;
+			sum++;
+		}
+		if(sum ==2 )
+			n2++;
+	    if ( sum ==3)
+			n3++;
+		if(sum >= 4)
+			n4++;
+		sum = 0;
+	
+		}
+		avg = avg/T;
+		System.out.println("Average: " + avg + " children to get at least one of each gender. ");
+		System.out.println("Number of families with 2 children: "+ n2);
+		System.out.println("Number of families with 3 children: " + n3);
+		System.out.println("Number of families with 4 children: "+ n4);
+		if(n2>=n3&&n2>=n4)
+			System.out.println("The most common number of children is 2 ");
+		else if (n3>=n2&&n3>=n4)	
+			System.out.println("The most common number of children is 3 ");
+		else
+			System.out.println("The most common number of children is 4 or more ");
 		//// In the previous version of this program, you used a statement like:
 		//// double rnd = Math.random();
 		//// Where "rnd" is the variable that stores the generated random value.
